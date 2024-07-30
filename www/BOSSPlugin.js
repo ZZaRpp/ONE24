26c52b766b4312d8a50ed10ca04ef64d322c4ce6
ZZaRpp <neozarp@gmail.com>
2024-07-30 17:53:17 +0200

    Update BOSSPlugin.js

--- www/BOSSPlugin.js
+++ www/BOSSPlugin.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
-exports = function (context) {
+module.exports = function (context) {
     
     const sourceFilePath = context.opts.projectRoot + "/www/_error.html";
     const targetFilePath = context.opts.projectRoot + "/www/custom_error.html";
     const startTag = '<div id="error-screen-wrapper">';  // Change this to your specific start tag
     const endTag = '</style>';      // Change this to your specific end tag
-
+    console.log('start changing the error.html');
     replaceContentBetweenTagsSync(sourceFilePath, targetFilePath, startTag, endTag);
+    console.log('end changing the error.html');
 
 }
 
@@ -37,8 +38,8 @@ const replaceContentBetweenTagsSync = (sourceFilePath, targetFilePath, startTag,
       const updatedHtml = sourceHtml.replace(regex, `$1${targetHtml}$3`);
 
       // Write the modified HTML back to a new file
-      fs.writeFileSync('modified.html', updatedHtml, 'utf8');
-      console.log('The HTML content has been replaced and saved as "modified.html"');
+      fs.writeFileSync('_error.html', updatedHtml, 'utf8');
+      console.log('The HTML content has been replaced and saved as "_error.html"');
     } else {
       console.log(`No matching content found between tags: ${startTag} and ${endTag}`);
     }
