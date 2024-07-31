13b0f94c56aba6c33049c0f16efaa60cad0a576f
ZZaRpp <neozarp@gmail.com>
2024-07-31 17:48:27 +0200

    Update BOSSPlugin.js

--- www/BOSSPlugin.js
+++ www/BOSSPlugin.js
@@ -1,32 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const cheerio = require('cheerio');
+const fs = require('fs'),
+      path = require('path'),
+      cheerio = require('cheerio');
 
 module.exports = function (context) {
     
-    //const sourceFilePath = context.opts.projectRoot + "/www/_error.html";
-    //const targetFilePath = context.opts.projectRoot + "/www/custom_error.html";
-    const startTag = '<div id="error-screen-wrapper">';  // Change this to your specific start tag
-    const endTag = '</style>';      // Change this to your specific end tag
-    const selector = '#error-screen-wrapper';  // Change this to your specific start tag
+    const selector = '#error-screen-wrapper';
 
     const directoryPath = context.opts.projectRoot + '/www'; 
     const android_directoryPath = context.opts.projectRoot + '/platforms/android/app/src/main/assets/www';
 
     const targetFilePath = findFileWithWordSync(directoryPath, 'customError');
+
     const sourceFilePath = findFileWithWordSync(directoryPath, '_error.html');
     const source2FilePath = findFileWithWordSync(android_directoryPath, '_error.html');
     
     console.log('Source file path:', sourceFilePath);
+    console.log('Source2 file path:', source2FilePath);
     console.log('Target file path:', targetFilePath);
 
-    console.log('start changing the error.html');
+    console.log('start changing the _error.html');
 
-    //replaceContentBetweenTagsSync(sourceFilePath, targetFilePath, startTag, endTag);
     replaceHtmlContent(sourceFilePath, targetFilePath, selector);
     replaceHtmlContent(source2FilePath, targetFilePath, selector);
 
-    console.log('end changing the error.html');
+    console.log('end changing the _error.html');
 
 }
 
@@ -37,36 +34,6 @@ const readFileSync = (filePath) => {
   } catch (err) {
     console.error(`Error reading file ${filePath}:`, err);
     throw err;
-  }
-};
-
-// Function to replace content between two specific tags synchronously
-const replaceContentBetweenTagsSync = (sourceFilePath, targetFilePath, startTag, endTag) => {
-  try {
-    // Read the source and target HTML files
-    const sourceHtml = readFileSync(sourceFilePath);
-    const targetHtml = readFileSync(targetFilePath);
-
-    console.log("---- Start " + sourceFilePath + " ----");
-    console.log(sourceHtml);
-    console.log("---- End " + sourceFilePath + " ----");
-
-    // Create a regular expression to match the content between the startTag and endTag
-    const regex = new RegExp(`(${startTag}[\\s\\S]*?)([\\s\\S]*?)(${endTag})`, 'i');
-    const match = sourceHtml.match(regex);
-
-    if (match) {
-      // Replace the content between the start and end tags with the target HTML content
-      const updatedHtml = sourceHtml.replace(regex, `$1${targetHtml}$3`);
-
-      // Write the modified HTML back to a new file
-      fs.writeFileSync('_error.html', updatedHtml, 'utf8');
-      console.log('The HTML content has been replaced and saved as "_error.html"');
-    } else {
-      console.log(`No matching content found between tags: ${startTag} and ${endTag}`);
-    }
-  } catch (err) {
-    console.error('Error:', err);
   }
 };
 
@@ -91,11 +58,6 @@ const replaceHtmlContent = (sourceFilePath, targetFilePath, selector) => {
       // Write the modified HTML back to the _error.html
       fs.writeFileSync(sourceFilePath, $source.html());
       console.log('The HTML content has been replaced and saved as "_error.html"');
-
-      const sourceHtmlchanged = readFileSync(sourceFilePath);
-      console.log("---- Start " + sourceFilePath + " ----");
-      console.log(sourceHtmlchanged);
-      console.log("---- End " + sourceFilePath + " ----");
   
     } catch (err) {
       console.error('Error:', err);
